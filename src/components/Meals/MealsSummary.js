54938e316ab76866e4d540f40f637309eaa321eb
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-10-06 17:40:28 +1100

    css

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,18 +1,23 @@
 import classes from './MealsSummary.module.css';
 
+import sceneImage from '../../assets/scene.svg'
+
 const MealsSummary = () => {
   return (
-    <section className={classes.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
-      <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
-      </p>
-    </section>
+    <div className={classes.mealssummary}>
+      <img src={sceneImage} alt='Scene of Order' />
+      <section className={classes.summary}>
+        <h2>Delicious Food, Delivered To You</h2>
+        <p>
+          Choose your favorite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home.
+        </p>
+        <p>
+          All our meals are cooked with high-quality ingredients, just-in-time and
+          of course by experienced chefs!
+        </p>
+      </section>
+    </div>
   );
 };
 
